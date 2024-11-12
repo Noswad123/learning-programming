939facc57221bdbb272482df339c07ccfd1747e9
Jamal Dawson <Jamal.Dawson.@modernatx.com>
2024-11-12 17:15:59 -0500

    fix: cleanup

--- learning-lua/conditions.lua
+++
@@ -1,9 +0,0 @@
-local condition = math.floor(100*math.random())
-
-if condition < 100 then
-	print(condition)
-end
-
-if condition%2 == 1 then
-	print('odd')
-end
--- learning-lua/hello.lua
+++
@@ -1,7 +0,0 @@
-local current = os.date()
--- run with lua hello.lua
-print('Hello lua')
-
---[[
-	Multiple line comment
-]]
